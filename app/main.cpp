55019f45f4c46cf204459298c70aa270b24ca99b
shivamsehgal77 <112571645+shivamsehgal77@users.noreply.github.com>
2023-10-10 16:24:37 -0400

    changed the test cases, PID gain values

--- app/main.cpp
+++ app/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-  PIDController controller_1 = PIDController(1.0, 1.0, 1.0);
+  PIDController controller_1 = PIDController(0.3, 0.9, 0.6);
   double actualVelocity = controller_1.actualVelocity(15.0, 10.0);
   std::cout<<"Actual Velocity : "<<actualVelocity<<std::endl;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -2,13 +2,13 @@
 #include "PID.hpp"
 
 TEST(first_test, this_should_pass) {
-  PIDController controller(0.1,0.01,0.2);
+  PIDController controller(0.3, 0.9, 0.6);
   double outout_velocity = controller.actualVelocity(15.0, 10.0);
   ASSERT_NEAR(15.0, outout_velocity, 1); 
 }
 
 TEST(second_test, this_should_pass) {
-  PIDController controller(0.1,0.01,0.2);
-  double outout_velocity = controller.actualVelocity(90.0, 100.0);
-  ASSERT_NEAR(90.0, outout_velocity, 1); 
+  PIDController controller(0.3, 0.9, 0.6);
+  double outout_velocity = controller.actualVelocity(50.0, 30.0);
+  ASSERT_NEAR(50.0, outout_velocity, 1); 
 }
