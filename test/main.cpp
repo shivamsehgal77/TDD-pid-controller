2eeb634ebd5cf91c7519769c3668fc924ef05de1
shivamsehgal77 <112571645+shivamsehgal77@users.noreply.github.com>
2023-10-10 16:33:56 -0400

    removed unwanted comments in test/main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -3,12 +3,6 @@
 #include "lib.hpp"
 #include "lib1.hpp"
 
-
-// void function_not_used() {
-//   dummy();
-//   int notUsed = my_function1(3);
-// }
-
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
