ecb9325104c110e447fda36da90eaa25985a3752
Shivam Sehgal <112571645+shivamsehgal77@users.noreply.github.com>
2023-10-10 20:17:46 -0400

    Updated main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,21 @@
-#include "PID.hpp"
+/**
+ * @file main.cpp
+ * @author Shivam Sehgal (ssehgal7@umd.edu)
+ * @brief A simple example of a PID controller.
+ * @version 0.1
+ * @date 2023-10-10
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include <iostream>
+#include "PID.hpp"
 
 int main() {
+  // object of class PIDController Created
   PIDController controller_1 = PIDController(0.3, 0.9, 0.6);
   double actualVelocity = controller_1.actualVelocity(15.0, 10.0);
-  std::cout<<"Actual Velocity : "<<actualVelocity<<std::endl;
+  // Display the result.
+  std::cout << "Actual Velocity : " << actualVelocity << std::endl;
 }
-
